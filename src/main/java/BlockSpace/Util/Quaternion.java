6a64767e3034728e5a72baf66168e4ec4e950831
hagane <deemson@gmail.com>
2012-09-19 09:56:38 +0500

    Started implementing quaternions

--- src/main/java/BlockSpace/Util/Quaternion.java
+++ src/main/java/BlockSpace/Util/Quaternion.java
@@ -6,11 +6,48 @@ package BlockSpace.Util;
  */
 public class Quaternion
 {
+    private float w;
+    private float x;
+    private float y;
+    private float z;
 
-    public Quaternion(float w, float x, float y, float z)
+    public Quaternion(float aW, float aX, float aY, float aZ)
     {
         throw new UnsupportedOperationException("Not yet implemented");
     }
+
+    /**
+     * @return scalar component
+     */
+    public float getW()
+    {
+        return w;
+    }
+
+    /**
+     * @return X component of vector
+     */
+    public float getX()
+    {
+        return x;
+    }
+
+    /**
+     * @return Y component of vector
+     */
+    public float getY()
+    {
+        return y;
+    }
+
+    /**
+     * @return Z component of vector
+     */
+    public float getZ()
+    {
+        return z;
+    }
+    
     @Override
     public boolean equals(Object obj)
     {
