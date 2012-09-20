984786a4ffabe4d73c66040741cea8219b147d70
hagane <deemson@gmail.com>
2012-09-20 13:48:00 +0500

    implemented the rest of quaternion methods

--- src/main/java/BlockSpace/Util/Quaternion.java
+++ src/main/java/BlockSpace/Util/Quaternion.java
@@ -98,27 +98,40 @@ public class Quaternion
 
     public Quaternion conjugate()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Quaternion(w,-x,-y,-z);
     }
 
     public float abs()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return (float) Math.sqrt(w*w+x*x+y*y+z*z);
     }
 
     public Quaternion multiply(float scalar)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return new Quaternion(scalar*w,scalar*x,scalar*y,scalar*z);
     }
 
     public Quaternion multiply(Quaternion other)
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        float nW = this.getW()*other.getW() - this.getX()*other.getX() 
+                - this.getY()*other.getY() - this.getZ()*other.getZ();
+        
+        float nX = this.getW()*other.getX() + this.getX()*other.getW() 
+                - this.getY()*other.getZ() + this.getZ()*other.getY();
+        
+        float nY = this.getW()*other.getY() + this.getX()*other.getZ() 
+                + this.getY()*other.getW() - this.getZ()*other.getX();
+        
+        float nZ = this.getW()*other.getZ() - this.getX()*other.getY() 
+                + this.getY()*other.getX() + this.getZ()*other.getW();
+        
+        return new Quaternion(nW, nX, nY, nZ);
     }
 
     public Quaternion normalize()
     {
-        throw new UnsupportedOperationException("Not yet implemented");
+        float length = this.abs();
+        return new Quaternion(w/length,x/length,y/length,z/length);
     }
     
 }
